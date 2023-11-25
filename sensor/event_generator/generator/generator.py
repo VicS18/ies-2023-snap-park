d75c661e624b3e6b66d310acee18c366831ab6a3
VicS18 <vitor.mtsantos@ua.pt>
2023-11-25 12:33:12 +0000

    [SNAP-14 Event Generation] Simulated event stream completed.

--- sensor/event_generator/generator/generator.py
+++ sensor/event_generator/generator/generator.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python
 import pika, json
 
-from sample_events import traffic, temperature_levels, light_levels, aq_levels
+from sample_events import TRAFFIC, TEMPERATURE_LEVELS, LIGHT_LEVELS, AQ_LEVELS
 
 QUEUE_NAME = 'snap_park'
 HOST = 'localhost'
 
-def send_event(event, queue_name, conn):
+def dummy(queue_name, conn):
     channel = conn.channel()
-    channel.queue_declare(queue='snap_park')
-    channel.basic_publish(exchange='', routing_key=queue_name, body=event)
+    channel.queue_declare(queue=queue_name)
+    for t in TRAFFIC:
+        channel.basic_publish(exchange='', routing_key=queue_name, body=json.dumps(t))
+    for tl in TEMPERATURE_LEVELS:
+        channel.basic_publish(exchange='', routing_key=queue_name, body=json.dumps(tl))
+    for ll in LIGHT_LEVELS:
+        channel.basic_publish(exchange='', routing_key=queue_name, body=json.dumps(ll))
+    for aql in AQ_LEVELS:
+        channel.basic_publish(exchange='', routing_key=queue_name, body=json.dumps(aql))
 
 def main():
     connection = pika.BlockingConnection(
     pika.ConnectionParameters(host=HOST))
+
+    dummy(QUEUE_NAME, connection)
 
     connection.close()
 
