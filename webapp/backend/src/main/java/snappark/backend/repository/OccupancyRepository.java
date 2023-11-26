9f9cabcc673ade85af345614ef2ab76387efdf33
VicS18 <vitor.mtsantos@ua.pt>
2023-11-26 20:18:27 +0000

    [Backend] Corrected repository to reference proper data type

--- webapp/backend/src/main/java/snappark/backend/repository/OccupancyRepository.java
+++ webapp/backend/src/main/java/snappark/backend/repository/OccupancyRepository.java
@@ -3,8 +3,7 @@ package snappark.backend.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import snappark.backend.entity.Occupancy;
-import snappark.backend.entity.Park;
 
-public interface OccupancyRepository extends JpaRepository<Occupancy, Park>{
+public interface OccupancyRepository extends JpaRepository<Occupancy, Long>{
     
 }
