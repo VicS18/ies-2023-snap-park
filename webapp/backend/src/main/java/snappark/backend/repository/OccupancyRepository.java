a22e6e8fd4111d7e30aba56175f1d46c9b952b1a
VicS18 <vitor.mtsantos@ua.pt>
2023-11-27 11:49:48 +0000

    [Backend] Compose for RabbitMQ, CockroachDB and Spring Boot

--- webapp/backend/src/main/java/snappark/backend/repository/OccupancyRepository.java
+++ webapp/backend/src/main/java/snappark/backend/repository/OccupancyRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import snappark.backend.entity.Occupancy;
 
 public interface OccupancyRepository extends JpaRepository<Occupancy, Long>{
-    
+
 }
