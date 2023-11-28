62f02bf8377a7fb64cbe00bd1e190d8f81279363
uTigas <tfgp@ua.pt>
2023-11-28 12:25:05 +0000

    Occupancy refactoring

--- webapp/backend/src/main/java/snappark/backend/service/OccupancyServiceImpl.java
+++ webapp/backend/src/main/java/snappark/backend/service/OccupancyServiceImpl.java
@@ -4,31 +4,23 @@ import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
-import snappark.backend.entity.Occupancy.OccupancyId;
 import snappark.backend.entity.Occupancy;
-import snappark.backend.entity.Park;
 import snappark.backend.repository.OccupancyRepository;
-import snappark.backend.repository.ParkRepository;
 
 @Service
 @AllArgsConstructor
 public class OccupancyServiceImpl implements OccupancyService {
     private OccupancyRepository occupancies;
-    private ParkRepository parks;
     public Occupancy getOccupancy(Long parkId){ 
-        Park park=parks.findById(parkId).orElse(null);
-        Occupancy occupancy = new Occupancy();
-        OccupancyId occupancyId = occupancy.new OccupancyId(park);
-
-        return occupancies.findById(occupancyId).orElse(null);    
+        return occupancies.findById(parkId).orElse(null);    
     }
 
     public Occupancy createOccupancy(Occupancy Occupancy){
         return occupancies.save(Occupancy);
     };
 
-    public Occupancy updateOccupancy(OccupancyId occupancyId, int change) {
-        Optional<Occupancy> optionalOccupancy = occupancies.findById(occupancyId);
+    public Occupancy updateOccupancy(Long parkID, int change) {
+        Optional<Occupancy> optionalOccupancy = occupancies.findById(parkID);
 
         if (optionalOccupancy.isPresent()) {
             Occupancy occupancy = optionalOccupancy.get();
