0e9d12f1caeb05549c2b1693423fd3c4d79a30f9
VicS18 <vitor.mtsantos@ua.pt>
2023-12-11 11:03:58 +0000

    [Frontend] Changed "Parks" button to "All Parks"

--- webapp/frontend/snappark/src/routes/+page.svelte
+++ webapp/frontend/snappark/src/routes/+page.svelte
@@ -17,7 +17,7 @@
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">Parks</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">All Parks</div>
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                             </div>
                         </div>
